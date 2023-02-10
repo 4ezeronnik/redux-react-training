1891b0201d8474175efa424c0712019c4cb4a39b
Andrii Reznichenko <4ezeronnik@gmail.com>
2023-02-10 11:30:56 +0200

    added details to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { Layout } from 'components/Layout/Layout';
+import { AppBar } from "./AppBar/AppBar";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <AppBar/>
+    </Layout>
   );
 };
