ef68d2ab9d03f17047b797fa0c1641180afafaae
Andrii Reznichenko <4ezeronnik@gmail.com>
2023-02-12 11:48:42 +0200

    added changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Layout } from 'components/Layout/Layout';
 import { AppBar } from "./AppBar/AppBar";
+import { TaskForm } from 'components/TaskForm/TaskForm';
 import { TaskList } from './TaskList/TaskList';
 
 export const App = () => {
   return (
     <Layout>
       <AppBar />
+      <TaskForm />
       <TaskList/>
     </Layout>
   );
